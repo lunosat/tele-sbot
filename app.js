0e505cc21b4b36a1a098a79e9cd2cba5cd478fc2
AkirahX <akirah.ofc@gmail.com>
2021-08-23 23:28:17 -0300

    Release

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
 const {Telegraf} = require('telegraf')
 const axios = require('axios')
 
-const bot = new Telegraf('1943842646:AAHx9LYfHnCoeK6DaDuGyeQUQei3biVu5yA')
+const bot = new Telegraf('1956985769:AAFn7XmAwlprW811yU-zbbDXO7QkW4gcOmU')
 
-bot.start((ctx) => ctx.reply('Olá, seja bem vindo'))
-bot.help((ctx) => ctx.reply('Quer ajuda em que se nem eu sei o que vou fazer?'))
+let startText = 'Olá, sejá bem vindo ao Image Maker, um simples bot desenvolvido para lhe entregar belas imagens\n\nAcompanhe nosso canal: t.me/SapphireNetwork'
+bot.start((ctx) => ctx.reply(startText))
+bot.help((ctx) => ctx.reply('Comandos:\n\n/love'))
 bot.on('sticker', (ctx) => ctx.reply('Ok'))
 bot.hears('hi', (ctx) => ctx.reply('Opa'))
+bot.telegram.
 bot.launch()
 
-bot.command('teste', (ctx) => {
-    try{
-        axios.get(`https://sapphire-api.herokuapp.com/api/textmaker/random?text=Alphabot&theme=art-quote&apikey=Alphabot`).then(res => {
-            //conn.sendFile(m.chat, res.data.result.url, 'glitch.jpg', 'Bye Oficial Sapphire API')
-            //ctx.reply(res.data.result.quotes)
-            ctx.replyWithPhoto(res.data.result.url)
-        })
-    } catch (err) {
-        console.log(err)
-    }
-    console.log('uu')
+bot.command('love', (ctx) => {
+    let query = ""
+    ctx.reply('Diga-me o texto que deseja.')
+    bot.on('text', (ctx) => {
+        query = ctx.message.text
+        query.toString()
+        try{
+            axios.get(`https://sapphire-api.herokuapp.com/api/textmaker/random?text=${query}&theme=art-quote&apikey=Alphabot`).then(res => {
+                ctx.replyWithPhoto(res.data.result.url)
+            })
+        } catch (err) {
+            console.log(err)
+        }
+        return
+    })
+    return
 })
 
 process.once('SIGINT', () => bot.stop('SIGINT'))
